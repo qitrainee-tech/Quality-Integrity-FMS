9131a2629fbcd3dfdb347570f4f1e0271d4ba840
qitrainee <qitrainee@gmail.com>
2026-02-02 13:33:42 +0800

    try

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: process.env.BASE_PATH || '/Quality-Integrity-FMS/tree/master',
+  base: process.env.VITE_BASE_PATH || '/Quality-Integrity-FMS/tree/master',
   resolve: {
     alias: {
       // Alias @ to the src directory
